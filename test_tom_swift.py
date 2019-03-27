8c003d7bf9d9df023065a21da5c65f7b1a16955a
Maxwell Brown <maxwellgbrown@gmail.com>
2019-03-27 14:32:11 -0400

    Implement compose_text

--- test_tom_swift.py
+++ test_tom_swift.py
@@ -4,7 +4,7 @@ import pytest
 import tom_swift
 
 
-@pytest.mark.parametrize("text,expected", [
+trigram_cases = [
     (
         "I wish I may I wish I might",
         {
@@ -14,7 +14,16 @@ import tom_swift
             ("I", "may"): ["I"],
         },
     )
-])
+]
+
+
+@pytest.mark.parametrize("text,expected", trigram_cases)
 def test_read_trigrams(text, expected):
     """Assert read_trigrams(text) reads the proper trigram."""
     assert tom_swift.read_trigrams(text) == expected
+
+
+@pytest.mark.parametrize("expected,trigram", trigram_cases)
+def test_compose_text(trigram, expected):
+    """Assert compose_text(trigram) write a valid text from trigram."""
+    assert tom_swift.compose_text(trigram) == expected
--- tom_swift.py
+++ tom_swift.py
@@ -18,10 +18,32 @@ def read_trigrams(text):
     return trigrams
 
 
+def _same_order(trigram):
+    """Yield items in the same order they were read from.
+
+    This should create a result equal to the original text.
+    """
+    keys = [k for k in trigram.keys()]  # keys are ordered since 3.6
+    seed = [*keys[0]]
+
+    yield from (item for item in seed)
+
+    while any(v for v in trigram.values()):
+        value = trigram[(seed[-2], seed[-1])].pop(0)
+        yield value
+        seed = [seed[-1], value]
+
+
+def compose_text(trigram, algorithm=_same_order):
+    """Compose text given a trigram."""
+    items = [i for i in algorithm(trigram)]
+    return " ".join(items)
+
+
 def main(text="I wish I may I wish I might"):
     """Execute the trigram."""
     trigrams = read_trigrams(text)
-    print(trigrams)
+    print(compose_text(trigrams))
 
 
 if __name__ == "__main__":
